9bb812caa9d309bda5dd76d81d1b922e3c8f42fd
Johann Listunov <johann.listunov@gmail.com>
2022-09-06 00:54:10 +0200

    Adds support for buffer autocomplete. Adds luasnip installation.

--- nvim/lua/custom/packer.lua
+++ nvim/lua/custom/packer.lua
@@ -2,14 +2,22 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
+
+    -- theme
     use 'folke/tokyonight.nvim'
 
     use 'nvim-lua/plenary.nvim'
     use 'nvim-lua/popup.nvim'
+
     use 'nvim-telescope/telescope-fzf-native.nvim'
     use 'nvim-telescope/telescope.nvim'
     
     use('nvim-treesitter/nvim-treesitter', {
         run = ':TSUpdate'
     })
+
+    use 'L3MON4D3/LuaSnip'
+    use 'saadparwaiz1/cmp_luasnip'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/nvim-cmp'
 end)
